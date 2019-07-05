70b75eea61e505fdee21e233f4ee97c1a4da8318
anohaker <anohaker@Mojave.lan>
2019-07-05 17:37:28 +0800

    fixef

--- contract/game.js
+++ contract/game.js
@@ -1,19 +1,46 @@
 "use strict";
 
 module.exports = {
-    start_period: async function () {
-        return "Contract[start_period] not implemented.";
+    start_period: async function (periodId) {
+        app.sdb.lock("game.period@" + periodId);
+        let exists = await app.model.Period.exists({ id: this.trs.id, periodId });
+        if (exists) return `periodId(${periodId}) already started.`;
+        app.sdb.create("GamePeriod", {
+            id: this.trs.id,
+            periodId,
+            status: 0
+        });
+        // return "Contract[start_period] not implemented.";
     },
 
     betting: async function () {
         return "Contract[betting] not implemented.";
     },
 
-    mothball_period: async function () {
-        return "Contract[mothball_period] not implemented.";
+    mothball_period: async function (periodId) {
+        app.sdb.lock("game.period@" + periodId);
+        let found = await app.model.GamePeriod.findAll({
+            fields: ["id", "status"],
+            condition: { periodId }
+        });
+        if (found.length != 1) return `periodId(${periodId}) not exists.`;
+        if (found[0].status !== 0) return `periodId(${periodId}) not in started status.`;
+
+        app.sdb.update("GamePeriod", { status: 1 }, { id: found[0].id, periodId });
+        // return "Contract[mothball_period] not implemented.";
     },
 
-    end_period: async function () {
-        return "Contract[end_period] not implemented.";
+    end_period: async function (periodId, points) {
+        app.sdb.lock("game.period@" + periodId);
+        let found = await app.model.GamePeriod.findAll({
+            fields: ["id", "status"],
+            condition: { periodId }
+        });
+        if (found.length !== 1) return `periodId(${periodId}) not exists.`;
+        if (found[0].status !== 1) return `periodId(${periodId}) not in mothball_period status.`;
+
+        app.sdb.update("GamePeriod", { status: 2, point_sequences: JSON.stringify(points.map(val => val.toString())) },
+            { id: found[0].id, periodId });
+        // return "Contract[end_period] not implemented.";
     }
 }
